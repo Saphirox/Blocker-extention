f54e850c32f1339275048f5ab27d249365770460
Maksym Suprunenko <suprmaks@gmail.com>
2018-12-09 12:45:13 +0200

    Added black screen

--- background.js
+++ background.js
@@ -1,6 +1,117 @@
 switcherTimeout = undefined;
 
+switcher = [];
+
+productivityHistory = [];
+
+lockerFree = true;
+
+function runTimer(url) {
+    return setInterval(() => {
+        chrome.storage.local.get(['active-manager'], (obj) => {
+            const activeMangerObj = obj['active-manager'];
+            activeMangerObj.currentTabName = url;
+           
+            const app = activeMangerObj.apps.filter(el => el.name == url);
+            
+            if (!app.length) {
+                
+                const appCategory = 
+                    activeMangerObj.predefinedCategories.find(el => el.origin == url);
+
+                if (appCategory)  {
+                    activeMangerObj.apps.push({ seconds: 0 , name: url, kind: appCategory.kind });
+                } 
+                else {
+                    activeMangerObj.apps.push({ seconds: 0 , name: url, kind: 'uncategorized' });
+                }
+
+                console.log(activeMangerObj.apps);
+                chrome.storage.local.set({ 'active-manager': activeMangerObj }, function() {
+                });
+            } else {
+                app[0].seconds++;
+                console.log(app.seconds);
+                chrome.storage.local.set({ 'active-manager': activeMangerObj }, function() {
+                });
+            }
+
+        });
+     }, 1000);
+}
+
+function sumProductivity() {
+}
+
+function sumProcrastination() {
+}
+
+function notifyProductivityIsDescrising(data) {
+    const procrastination = sumProcrastination();
+    const productivity = sumProductivity();
+
+    productivityHistory.push({
+        productivityPercent: procrastination,
+        productivityPercent: productivity,
+    });
+}
+
+function getCurrentApp(activeMangerObj) {
+    return activeMangerObj.apps.find(el => el.name == activeMangerObj.currentTabName);
+}
+
+function notifyAboutSwitchingActivity(data) {
+    const maxSiteRedirect = 5;
+
+    if (switcher.length > maxSiteRedirect){
+        const hasSwitchingProblem = switcher.map(c => c.seconds).reduce((acc, curr) => acc + curr, 0) 
+        / switcher.length < 20;
+
+        if (hasSwitchingProblem) {
+            new Notification("You swipe so much time, try to relax");
+            switcher = [];
+        }
+    }
+
+    chrome.storage.local.get(['active-manager'], (obj) => {
+        const data = obj['active-manager'];
+
+        if (data.currentTabName) {
+            switcher.push({
+                hostname: data.currentTabName,
+                seconds: getCurrentApp(data).seconds
+            });
+        }
+    });
+}
+
+function checkForNotifications(data) 
+{
+    notifyAboutSwitchingActivity(data);
+}
+
+function showNotification() {
+    chrome.browserAction.onClicked.addListener(function() {
+        chrome.notifications.create("", {
+            type:    "basic",
+            title:   "REMINDER",
+            message: "It's time to go to this super-cool site !\nProceed ?",
+            contextMessage: "It's about time...",
+            buttons: [{
+                title: "Yes, get me there",
+            }, {
+                title: "Get out of my way",
+            }]
+        }, function(id) {
+            myNotificationID = id;
+        });
+    });
+
+
+}
+
 chrome.runtime.onInstalled.addListener(() => {
+    showNotification();
     const url = chrome.runtime.getURL('predefined-sites.json');
 
     fetch(url)
@@ -15,10 +126,14 @@ chrome.runtime.onInstalled.addListener(() => {
 });
 });
 
-chrome.tabs.onHighlighted.addListener(function(highlightInfo) {
-    
-    chrome.tabs.query({ active: true },function(tabArr) {
-        
+function paretoPrincipleWorks(prod, proc) {
+    const per100 = (prod + proc);
+    const per80 = per100 * 0.7;
+    return per80 < prod;
+}
+
+function Main() {
+    chrome.tabs.query({ highlighted: true },function(tabArr) {
         const tab = tabArr[0];
 
         if (switcherTimeout) {
@@ -28,35 +143,59 @@ chrome.tabs.onHighlighted.addListener(function(highlightInfo) {
         let url = new URL(tab.url).hostname;
         console.log(url);
 
-        switcherTimeout = setInterval(() => {
-            chrome.storage.local.get(['active-manager'], (obj) => {
-                const activeMangerObj = obj['active-manager'];
-                activeMangerObj.currentTabName = url;
-               
-                const app = activeMangerObj.apps.filter(el => el.name == url);
-                
-                if (!app.length) {
-                    
-                    const appCategory = 
-                        activeMangerObj.predefinedCategories.find(el => el.origin == url);
+        switcherTimeout = runTimer(url);
+        
+        setTimeout(c => {
+        chrome.storage.local.get(['active-manager'], (obj) => {
+            const apps = obj['active-manager'].apps;
 
-                    if (appCategory)  {
-                        activeMangerObj.apps.push({ seconds: 0 , name: url, kind: appCategory.kind });
-                    } 
-                    else {
-                        activeMangerObj.apps.push({ seconds: 0 , name: url, kind: 'uncategorized' });
-                    }
+            if(!obj['active-manager'].currentTabName) {
+                return;
+            }
 
-                    console.log(activeMangerObj.apps);
-                    chrome.storage.local.set({ 'active-manager': activeMangerObj }, function() {
-                    });
-                } else {
-                    app[0].seconds++;
-                    console.log(app.seconds);
-                    chrome.storage.local.set({ 'active-manager': activeMangerObj }, function() {
-                    });
-                }
-            });
-        }, 1000);
+            const needToBlocked = 
+                apps.find(c => 
+                    c.kind === 'procrastination' && c.name == obj['active-manager'].currentTabName);
+        
+            const prod = 
+                apps.filter(c => c.kind === 'productivity').map(c => c.seconds).reduce((acc, curr) => acc + curr, 0);
+        
+            const proc = 
+                apps.filter(c => c.kind === 'procrastination').map(c => c.seconds).reduce((acc, curr) => acc + curr, 0);
+
+            if (needToBlocked && !paretoPrincipleWorks(prod, proc)) {
+                chrome.tabs.sendMessage(tab.id, {"sadasd": "asda"  });
+            }
+        });
+        }, 2000)
+
+
+        chrome.storage.local.get(['active-manager'], (obj) => {
+            checkForNotifications(obj['active-manager']);
+        });
     });
+}
+
+function lock() {
+    if (lockerFree) {
+        lockerFree = false;
+        Main(); 
+        lockerFree = true;
+    }
+}
+
+chrome.tabs.onActivated.addListener(function(highlightInfo) {
+    lock();
+});
+
+chrome.tabs.onHighlighted.addListener(function(highlightInfo) {
+    lock();
+});
+
+chrome.tabs.onCreated.addListener(function(highlightInfo) {
+    lock();
+});
+
+chrome.tabs.onUpdated.addListener(function(highlightInfo) {
+    lock();
 });
--- popup.js
+++ popup.js
@@ -1,34 +1,24 @@
-function drawCharts(activityManager) {
-    console.log(activityManager);
-    const apps = activityManager.apps;
-    
-    const productivitySeconds = 
-    apps.filter(c => c.kind === 'productivity').map(c => c.seconds).reduce((acc, curr) => acc + curr, 0);
 
-    const procrastinationSeconds = 
-    apps.filter(c => c.kind === 'procrastination').map(c => c.seconds).reduce((acc, curr) => acc + curr, 0);
-
-    var ctx = document.getElementById("myChart");
+function drawBar(id, s1, label, color) {
+    var ctx = document.getElementById(id).getContext('2d');
     var myChart = new Chart(ctx, {
-    type: 'bar',
+    type: 'horizontalBar',
     data: {
-        labels: ["Productivity", "Procrastination"],
+        labels: s1.map(c => c.name),
         datasets: [{
-            label: '# of Votes',
-            data: [productivitySeconds, procrastinationSeconds],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-            ],
-            borderWidth: 1
+            label: label,
+            data: s1.map(c => c.seconds),
+            backgroundColor: s1.map(c => c.seconds).map(color),
+            borderColor: s1.map(c => c.seconds).map(color)
         }]
     },
     options: {
         scales: {
+            xAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }],
             yAxes: [{
                 ticks: {
                     beginAtZero: true
@@ -39,8 +29,42 @@ function drawCharts(activityManager) {
 });
 }
 
+function drawCharts(activityManager) {
+    console.log(activityManager);
+    const apps = activityManager.apps;
+
+    const s1 = apps.filter(c => c.kind === 'productivity');
+    const s2 = apps.filter(c => c.kind === 'procrastination');
+
+    const productivitySeconds = 
+    apps.filter(c => c.kind === 'productivity').map(c => c.seconds).reduce((acc, curr) => acc + curr, 0);
+
+    const procrastinationSeconds = 
+    apps.filter(c => c.kind === 'procrastination').map(c => c.seconds).reduce((acc, curr) => acc + curr, 0);
+
+    drawBar("canvas1", s1, "Productivity", () =>
+    `hsla(74, 100%, ${Math.floor(Math.random() * 80) + 20}%, 0.7)`);
+    drawBar("canvas2", s2, "Procrastination",
+    () =>`hsla(0, 100%, ${Math.floor(Math.random() * 80) + 20}%, 0.7)`);
+
+    document.getElementById("productivity").innerHTML = productivitySeconds;
+    document.getElementById("procrastination").innerHTML = procrastinationSeconds;
+}
+
 window.onload = function () {
     chrome.storage.local.get(['active-manager'], (obj) => {
         drawCharts(obj['active-manager']);
     });
 }
+
+chrome.runtime.onMessage.addListener(function(message, callback) {
+    console.log("Changeds");
+    document.body.innerHTML= "<p>Your time expired<p>";
+    document.body.style.height = "768px";
+    var s = document.getElementsByTagName('p')[0];
+    s.style.fontSize = "46px";
+    s.style.color = "#FFF";
+    s.style.textAlign = "center";
+    s.style.verticalAlign = 'middle';
+    document.body.style.background = "#000";
+});
